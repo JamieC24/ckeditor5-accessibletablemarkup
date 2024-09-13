42ed545e731abc0d5471cc0769f348158a35e3bb
JamieC24 <jamie@studio24.net>
2024-09-13 09:02:20 +0100

    Remove linting

--- src/rnibtablemarkup.js
+++ src/rnibtablemarkup.js
@@ -1,40 +1,150 @@
-import { Plugin } from 'ckeditor5/src/core';
-import { ButtonView } from 'ckeditor5/src/ui';
-
-import ckeditor5Icon from '../theme/icons/ckeditor.svg';
+import { Plugin, TableEditing } from 'ckeditor5';
 
 export default class Rnibtablemarkup extends Plugin {
+	static get requires() {
+		return [TableEditing];
+	}
+
 	static get pluginName() {
 		return 'Rnibtablemarkup';
 	}
 
 	init() {
 		const editor = this.editor;
-		const t = editor.t;
-		const model = editor.model;
 
-		// Add the "rnibtablemarkupButton" to feature components.
-		editor.ui.componentFactory.add( 'rnibtablemarkupButton', locale => {
-			const view = new ButtonView( locale );
+		editor.conversion.for('downcast').add((dispatcher) => {
+			dispatcher.on('insert:table', (evt, data, conversionApi) => {
+				const viewWriter = conversionApi.writer;
+				const modelTable = data.item;
 
-			view.set( {
-				label: t( 'Rnibtablemarkup' ),
-				icon: ckeditor5Icon,
-				tooltip: true
-			} );
+				// Ensure the modelTable is correctly mapped to a view element
+				let tableWrapper =
+					conversionApi.mapper.toViewElement(modelTable);
+				if (tableWrapper && tableWrapper.name === 'figure') {
+					// Save the contentToolbar configuration
+					const contentToolbar = editor.config.get(
+						'table.contentToolbar'
+					);
+					// Create a new div element
+					const div = viewWriter.createContainerElement('div', {
+						class: 'table-wrap',
+					});
 
-			// Insert a text into the editor after clicking the button.
-			this.listenTo( view, 'execute', () => {
-				model.change( writer => {
-					const textNode = writer.createText( 'Hello CKEditor 5!' );
+					// Move all children from the figure to the div
+					while (tableWrapper.getChild(0)) {
+						const child = tableWrapper.getChild(0);
+						viewWriter.move(
+							viewWriter.createRangeOn(child),
+							viewWriter.createPositionAt(div, 'end')
+						);
+					}
 
-					model.insertContent( textNode );
-				} );
+					// Copy attributes and classes from the figure to the div
+					for (const attribute of tableWrapper.getAttributes()) {
+						viewWriter.setAttribute(
+							attribute[0],
+							attribute[1],
+							div
+						);
+					}
+					for (const className of tableWrapper.getClassNames()) {
+						viewWriter.addClass(className, div);
+					}
 
-				editor.editing.view.focus();
-			} );
+					// Copy custom properties from the figure to the div
+					const customProperties = tableWrapper.getCustomProperties();
+					for (const [key, value] of customProperties) {
+						viewWriter.setCustomProperty(key, value, div);
+					}
 
-			return view;
-		} );
+					// Replace the figure with the div
+					viewWriter.insert(
+						viewWriter.createPositionBefore(tableWrapper),
+						div
+					);
+					viewWriter.remove(tableWrapper);
+					// Update the mapping in the conversion API
+					conversionApi.mapper.bindElements(modelTable, div);
+				}
+			});
+		});
+	}
+	afterInit() {
+		const editor = this.editor;
+		// Handle the insertion of table captions
+		editor.conversion.for('downcast').add((dispatcher) => {
+			dispatcher.on('insert:caption', (evt, data, conversionApi) => {
+				const viewWriter = conversionApi.writer;
+				const modelCaption = data.item;
+
+				// Ensure the modelCaption is correctly mapped to a view element
+				let captionWrapper =
+					conversionApi.mapper.toViewElement(modelCaption);
+				if (captionWrapper && captionWrapper.name === 'figcaption') {
+					const captionTag = viewWriter.createContainerElement(
+						'caption',
+						{
+							class: 'table-caption',
+						}
+					);
+
+					while (captionWrapper.getChild(0)) {
+						const child = captionWrapper.getChild(0);
+						viewWriter.move(
+							viewWriter.createRangeOn(child),
+							viewWriter.createPositionAt(captionTag, 'end')
+						);
+					}
+
+					for (const attribute of captionWrapper.getAttributes()) {
+						viewWriter.setAttribute(
+							attribute[0],
+							attribute[1],
+							captionTag
+						);
+					}
+					for (const className of captionWrapper.getClassNames()) {
+						viewWriter.addClass(className, captionTag);
+					}
+
+					const captionCustomProperties =
+						captionWrapper.getCustomProperties();
+					for (const [key, value] of captionCustomProperties) {
+						viewWriter.setCustomProperty(key, value, captionTag);
+					}
+
+					const model = editor.model;
+					let tableElement = null;
+					model.change((writer) => {
+						const root = model.document.getRoot();
+						for (const child of root.getChildren()) {
+							if (child.is('element', 'table')) {
+								tableElement = child;
+								break;
+							}
+						}
+					});
+
+					if (tableElement) {
+						viewWriter.insert(
+							viewWriter.createPositionAt(
+								conversionApi.mapper.toViewElement(
+									tableElement
+								),
+								0
+							),
+							captionTag
+						);
+						viewWriter.remove(captionWrapper);
+						conversionApi.mapper.bindElements(
+							modelCaption,
+							captionTag
+						);
+					} else {
+						console.log('Table element not found for caption');
+					}
+				}
+			});
+		});
 	}
 }
